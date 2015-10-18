1b64053dbdb3dc87ea571f7988d8b4a95a23ebb6
Prelang Builder <builder@prelang.com>
2015-10-18 12:45:11 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Game' to allow for create, update

--- app/admin/game.rb
+++ app/admin/game.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Game do
+  permit_params :name, :status, :image
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
